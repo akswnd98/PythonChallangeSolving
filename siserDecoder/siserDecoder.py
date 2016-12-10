5a740782ee91f4764f3c1a6cfcbb5a398c51ab86
kim manjung <kim manjung>
2016-12-10 15:02:44 +0900

    first

--- siserDecoder/siserDecoder.py
+++ siserDecoder/siserDecoder.py
@@ -1,16 +1,16 @@
 def decode_siser_str(str_encoded, siser_key):
     if siser_key < 0:
         siser_key = siser_key % 26
-        
-    str_encoded_list = list(str_encoded)
-    i = 0
-    n = 0
+    
+    str_decoded = ""
     m = 0
-    for x in str_encoded_list:
+    v = 0
+    for x in str_encoded:
         n = ord(x)
-        m = n
-        v = 0
-        n += siser_key
+        if x.isalpha():
+            m = n
+            v = 0
+            n += siser_key
 
         if (m >= ord("A")) and (m <= ord("Z")):
             if n > ord("Z"):
@@ -24,7 +24,6 @@ def decode_siser_str(str_encoded, siser_key):
                 v = v % 26
                 n = ord("a") + v - 1
                     
-        str_encoded_list[i] = chr(n)
-        i += 1
+        str_decoded = str_decoded + chr(n)
 
-    return "".join(str_encoded_list)
+    return str_decoded
